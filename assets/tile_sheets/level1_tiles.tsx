3149f47b7c7b0518fd8453b0acee7d121f822665
jsmal-lab <jsmall@stu.kerikerihigh.ac.nz>
2021-06-15 00:18:20 +1200

    create  assets folder and map folder with level 1 tile sheet

---
+++ assets/tile_sheets/level1_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="level1_tiles" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="level1_tiles.png" width="512" height="512"/>
+</tileset>
