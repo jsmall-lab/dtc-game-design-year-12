49f7942782b06ccfb415f4d758a465c833d33998
jsmall-lab <82071548+jsmall-lab@users.noreply.github.com>
2021-08-08 18:48:20 +1200

    add i added level2 tileset

---
+++ assets/tile_sheets/level2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="level2" tilewidth="128" tileheight="128" tilecount="24" columns="6">
+ <image source="level2_tiles.png" width="768" height="512"/>
+</tileset>
