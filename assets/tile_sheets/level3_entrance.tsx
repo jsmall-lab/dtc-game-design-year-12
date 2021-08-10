f3c34b8530638002b840857842e809b0e71c2f88
jsmall-lab <82071548+jsmall-lab@users.noreply.github.com>
2021-08-10 19:47:18 +1200

    add level3 entrance
    add level3 and runs on game

---
+++ assets/tile_sheets/level3_entrance.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="level3_entrance" tilewidth="128" tileheight="128" tilecount="30" columns="6">
+ <image source="level3_tiles.png.png" width="768" height="640"/>
+</tileset>
