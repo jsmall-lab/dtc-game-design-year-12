070acb64db82cb32aae9d4bc6c84a11708eb1d02
jsmall-lab <82071548+jsmall-lab@users.noreply.github.com>
2021-08-12 19:37:20 +1200

    moved views to views folder ready fo use

--- views/start_view.py
+++ views/start_view.py
@@ -5,21 +5,21 @@ HEIGHT = 800
 
 
 class StartView(arcade.View):
+      
 
     def on_show(self):
-        arcade.set_background_color(arcade.color.AERO_BLUE)
+        arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
         arcade.set_viewport(0, WIDTH -1, 0, HEIGHT -1)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Start Screen", WIDTH / 2, HEIGHT / 2, arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Start Screen", WIDTH / 2, HEIGHT / 2, arcade.color.BLACK, font_size=50, anchor_x="center")
         
-        arcade.draw_text("Click to Start", WIDTH / 2, HEIGHT / 2 - 75, arcade.color.WHITE, font_size=20, anchor_x="center")
+        arcade.draw_text("Click to Start", WIDTH / 2, HEIGHT / 2 - 75, arcade.color.BLACK, font_size=20, anchor_x="center")
         arcade.finish_render()
     
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, start the game. """
-        game_view = Game()
+        game_view = self.window.game_view
         game_view.setup()
         self.window.show_view(game_view)
-
