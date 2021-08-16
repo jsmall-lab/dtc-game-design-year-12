026df617ac2a59f9ea05eb1d6bcfb9e371c820c9
jsmall-lab <82071548+jsmall-lab@users.noreply.github.com>
2021-08-16 21:28:15 +1200

    make bullet pick up image
    make health pick up image
    
    make gem pick up image

---
+++ assets/consumables/bullet_pickup.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="bullet_pickup" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="bullet_pickup.png" width="64" height="64"/>
+</tileset>
---
+++ assets/consumables/gem.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="gem" tilewidth="96" tileheight="96" tilecount="1" columns="1">
+ <image source="gem.png" width="96" height="96"/>
+</tileset>
